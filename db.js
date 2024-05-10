c33a03846e469b743c50bc283cdd37377e28b28a
Brian Ramsay <ramsayb2@berea.edu>
2024-05-10 15:21:17 +0000

    sqlite library properly creating a database file.

--- db.js
+++ db.js
@@ -3,14 +3,42 @@ import env from "dotenv";
 
 env.config();
 
-const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-});
+import fs from "fs";
+const dirPath = "./.data"
+if(!fs.existsSync(dirPath)) { 
+  fs.mkdirSync(dirPath);
+}
+const dbFile = `${dirPath}/bwc.db`;
+const exists = fs.existsSync(dbFile);
 
-db.connect();
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+//SQLite wrapper for async / await connections https://www.npmjs.com/package/sqlite
+let db;
+open({
+    filename: dbFile,
+    driver: sqlite3.Database
+  })
+  .then(async dbConn => {
+    db = dbConn;
+
+    try {
+      if (!exists) {
+
+        // Run tables.sql
+        await db.run(
+          "CREATE TABLE Messages (id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT)"
+        );
+        // Insert data
+        await db.run(
+          "INSERT INTO Messages (message) VALUES (?)", "tmp"
+        );
+      }
+      console.log(await db.all("SELECT * from Messages"));
+    } catch (dbError) {
+      console.error(dbError);
+    }
+  });
 
 export default db;
